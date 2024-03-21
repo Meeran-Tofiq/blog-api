4cae7143add2342b7946463b1f1afdfcbfd919cb
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-21 15:35:03 +0300

    Feat model representing comments

---
+++ models/comment.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Comment = new Schema(
+	{
+		user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+		post: { type: Schema.Types.ObjectId, required: true, ref: "BlogPost" },
+		date: { type: Date, required: true, default: Date.now() },
+	},
+	{
+		virtuals: {
+			date: {
+				get() {
+					return DateTime.fromJSDate(this.due_back).toLocaleString(
+						DateTime.DATETIME_MED
+					);
+				},
+			},
+		},
+	}
+);
+
+module.exports = mongoose.model("User", User);
