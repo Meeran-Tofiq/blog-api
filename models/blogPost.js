cbe719642bab6b63916369775a260b8f9427adb2
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-21 15:35:16 +0300

    Feat model representing blog posts

---
+++ models/blogPost.js
@@ -0,0 +1,25 @@
+const { DateTime } = require("luxon");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BlogPost = new Schema(
+	{
+		user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+		title: { type: String, required: true, minLength: 3 },
+		content: { type: String, required: true, minLength: 3 },
+		date: { type: Date, required: true, default: Date.now() },
+	},
+	{
+		virtuals: {
+			date: {
+				get() {
+					return DateTime.fromJSDate(this.due_back).toLocaleString(
+						DateTime.DATETIME_MED
+					);
+				},
+			},
+		},
+	}
+);
+
+module.exports = mongoose.model("BlogPost", BlogPost);
