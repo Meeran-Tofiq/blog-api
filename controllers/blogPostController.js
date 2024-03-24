db5f697c53aea7550c6de094b7a09e3f61a8f620
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-24 15:05:32 +0300

    Feat handler for deleting a blog post

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -136,6 +136,46 @@ blogPostController.putBlogPost = [
 	}),
 ];
 
-blogPostController.deleteBlogPost = asyncHandler((req, res, next) => {});
+blogPostController.deleteBlogPost = asyncHandler(async (req, res, next) => {
+	const errors = validationResult(req);
+
+	if (errors) {
+		res.send(400).json({ errors });
+	}
+
+	try {
+		const decoded = await jwtUtil.verifyWebToken(req.token);
+		const user = decoded.user;
+		const blogPostToDelete = await BlogPost.findById(
+			req.params.blogPostId
+		).exec();
+
+		if (!blogPostToDelete) {
+			throw new Error("CastError");
+		}
+
+		if (blogPostToDelete.user !== user.id) {
+			throw new Error("Unauthorized");
+		}
+
+		await BlogPost.deleteOne({ _id: req.params.blogPostId });
+		res.sendStatus(200);
+	} catch (error) {
+		if (
+			error.name === "JsonWebTokenError" ||
+			error.name === "TokenExpiredError"
+		) {
+			res.status(403).json("Invalid or expired token.");
+		} else if (error.message === "Unauthorized") {
+			res.status(403).json(
+				"User is not authorized to delete this blog post."
+			);
+		} else if (error.name === "CastError") {
+			res.status(404).json("Blog post not found.");
+		} else {
+			res.status(500).json("Internal server error.");
+		}
+	}
+});
 
 module.exports = blogPostController;
