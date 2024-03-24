5a89642731256022264350d54d66e19836b5977f
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-24 14:30:02 +0300

    Feat handler for posting a blog post

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -1,10 +1,55 @@
 const BlogPost = require("../models/blogPost");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const jwtUtil = require("../util/jwtUtil");
 
 let blogPostController = {};
 
-blogPostController.postBlogPost = asyncHandler((req, res, next) => {});
+blogPostController.postBlogPost = [
+	body("title")
+		.trim()
+		.isLength({ min: 3 })
+		.escape()
+		.withMessage("Title must be at least 3 characters."),
+	body("content")
+		.trim()
+		.isLength({ min: 3 })
+		.escape()
+		.withMessage("content must be at least 3 characters."),
+	,
+	body("isPublished")
+		.isBoolean()
+		.withMessage("Choose whether this post is published or not."),
+	,
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+
+		if (errors) {
+			res.send(400).json({ errors });
+		}
+
+		try {
+			const decoded = await jwtUtil.verifyWebToken(req.token);
+			const user = decoded.user;
+
+			if (!user.canPost) {
+				throw new Error();
+			}
+
+			const blogPost = new BlogPost({
+				user: user._id,
+				title: req.body.title,
+				content: req.body.content,
+				isPublished: req.body.isPublished,
+			});
+
+			await blogPost.save();
+			res.send(200);
+		} catch (error) {
+			res.send(403).json("Unable to verify user, or user can't post.");
+		}
+	}),
+];
 
 blogPostController.getBlogPost = asyncHandler((req, res, next) => {});
 
