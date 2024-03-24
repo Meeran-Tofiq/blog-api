fb5d71fee211064c048d6be0d9c02c6ae0cda50a
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-24 13:58:04 +0300

    Feat user login, with providing a json web token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 let userController = {};
 
@@ -11,5 +12,34 @@ userController.getUser = asyncHandler((req, res, next) => {});
 userController.putUser = asyncHandler((req, res, next) => {});
 
 userController.deleteUser = asyncHandler((req, res, next) => {});
+
+userController.login = [
+	body("username")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("Username must not be empty."),
+	body("password")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("Password must not be empty."),
+	asyncHandler(async (req, res, next) => {
+		const user = User.find({
+			username: req.body.username,
+			password: req.body.password,
+		}).exec();
+
+		if (!user) {
+			res.status(401).send("Invalid username or password.");
+		}
+
+		res.json(generateWebToken(user));
+	}),
+];
+
+function generateWebToken(user) {
+	return jwt.sign({ user }, process.env.JWT_SECRET);
+}
 
 module.exports = userController;
