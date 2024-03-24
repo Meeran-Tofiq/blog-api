81d47f7dddf8c9605e7a97329f1bb0928ff31f37
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-24 17:16:23 +0300

    Remove unnecessary validation result

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -156,12 +156,6 @@ blogPostController.putBlogPost = [
 ];
 
 blogPostController.deleteBlogPost = asyncHandler(async (req, res, next) => {
-	const errors = validationResult(req);
-
-	if (!errors.isEmpty()) {
-		return res.status(400).json({ errors: errors.array() });
-	}
-
 	try {
 		const decoded = await jwtUtil.verifyWebToken(req.token);
 		const user = decoded.user;
