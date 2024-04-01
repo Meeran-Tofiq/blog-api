a5c232fec052054782a766441b649eea83f8f258
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-04-01 12:45:34 +0300

    Feat route handling for comment of a blog post

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+const commentController = require("../controllers/commentController");
 
-router.get("/", function (req, res, next) {
-	res.send("COMMENT: respond with a resource");
-});
+// CREATE COMMENT
+router.post("/", commentController.postComment);
+
+// READ COMMENT(S)
+router.get("/", commentController.getMultipleComments);
+router.get("/:commentId", commentController.getComment);
+
+// UPDATE COMMENT
+router.put("/:commentId", commentController.putComment);
+
+// DELETE COMMENT
+router.delete("/:commentId", commentController.deleteComment);
 
 module.exports = router;
