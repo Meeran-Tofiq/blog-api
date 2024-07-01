631986f14df8c5f04e8a243bf16157a1a526926c
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-07-01 10:25:10 +0300

    Feat disconnection from mongo server

--- mongoTesting.config.js
+++ mongoTesting.config.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 
+let mongoServer;
+
 async function initializeMongoServer() {
-	const mongoServer = await MongoMemoryServer.create();
+	mongoServer = await MongoMemoryServer.create();
 	const mongoUri = mongoServer.getUri();
 
 	mongoose.connect(mongoUri);
@@ -20,4 +22,9 @@ async function initializeMongoServer() {
 	});
 }
 
-module.exports = initializeMongoServer;
+async function disconnectMongoServer() {
+	await mongoose.disconnect();
+	await mongoServer.stop();
+}
+
+module.exports = { initializeMongoServer, disconnectMongoServer };
