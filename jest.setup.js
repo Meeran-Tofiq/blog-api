84ff2f03b1219037a2743bffda06d95a6f24b166
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-07-02 16:11:42 +0300

    Feat mocking data before tests begin

--- jest.setup.js
+++ jest.setup.js
@@ -7,6 +7,9 @@ const {
 const apiRouter = require("./routes/api");
 const { extractToken } = require("./util/jwtUtil");
 const express = require("express");
+const { createUserMocks } = require("./__mocks__/users.mock");
+const { createBlogPostMocks } = require("./__mocks__/blogPosts.mock");
+const { createCommentMocks } = require("./__mocks__/comments.mock");
 
 let app;
 
@@ -15,9 +18,13 @@ beforeAll(async () => {
 
 	app = express();
 	app.use(express.urlencoded({ extended: false }));
-	app.use(express.json()); // Add this if you need JSON parsing
+	app.use(express.json());
 	app.use(extractToken);
 	app.use("/api", apiRouter);
+
+	await createUserMocks();
+	await createBlogPostMocks();
+	await createCommentMocks();
 });
 
 afterAll(async () => {
