2be77c536964d76415af8b25e6d203a2131a35e0
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-07-04 11:38:12 +0300

    Test getting all comments under a post

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -1,22 +1,47 @@
 const { comments } = require("../__mocks__/comments.mock");
-const apiRouter = require("../routes/api");
+const { blogPosts } = require("../__mocks__/blogPosts.mock");
 const request = require("supertest");
-const express = require("express");
-const app = express();
+const app = require("../jest.setup");
 
-app.use(express.urlencoded({ extended: false }));
-app.use("/api", apiRouter);
-
-const baseUrl = "/api/comments";
+const baseUrl = (blogPostId) => `/api/blog-posts/${blogPostId}/comments`;
 
 describe("Comments route", () => {
 	describe("GET", () => {
 		it("should return all comments, if a comment is not specified", async () => {
-			request(app)
-				.get(baseUrl)
+			const blogPostId = blogPosts[0]._id.toString();
+
+			const response = await request(app)
+				.get(baseUrl(blogPostId))
 				.expect("Content-Type", /json/)
-				.expect({ data: comments })
 				.expect(200);
+
+			const responseBody = response.body.data.map((comment) => ({
+				_id: comment._id.toString(),
+				user: {
+					_id: comment.user._id.toString(),
+				},
+				blogPost: comment.blogPost.toString(),
+				content: comment.content,
+				date: new Date(comment.date).toISOString(),
+				isEdited: comment.isEdited,
+				__v: comment.__v,
+			}));
+
+			const expectedBody = comments
+				.filter((comment) => comment.blogPost.toString() === blogPostId)
+				.map((comment) => ({
+					_id: comment._id.toString(),
+					user: {
+						_id: comment.user.toString(),
+					},
+					blogPost: comment.blogPost.toString(),
+					content: comment.content,
+					date: comment.date.toISOString(),
+					isEdited: comment.isEdited,
+					__v: comment.__v,
+				}));
+
+			expect(responseBody).toEqual(expectedBody);
 		});
 	});
 });
