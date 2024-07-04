7c9237d059c43333e4cde1806332838a44b8d865
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-07-04 11:13:22 +0300

    Feat test for getting all posts

--- __tests__/blogPosts.test.js
+++ __tests__/blogPosts.test.js
@@ -1,22 +1,35 @@
 const { blogPosts } = require("../__mocks__/blogPosts.mock");
-const apiRouter = require("../routes/api");
 const request = require("supertest");
-const express = require("express");
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use("/api", apiRouter);
+const mongoose = require("mongoose");
+const app = require("../jest.setup");
 
 const baseUrl = "/api/blog-posts";
 
 describe("Blog Posts route", () => {
 	describe("GET", () => {
 		it("should return all blog posts, if a blog post is not specified", async () => {
-			request(app)
+			const response = await request(app)
 				.get(baseUrl)
 				.expect("Content-Type", /json/)
-				.expect({ data: blogPosts })
 				.expect(200);
+
+			const responseBody = response.body.data.map((post) => ({
+				...post,
+				_id: post._id.toString(),
+				date: new Date(post.date),
+				user: post.user._id.toString(),
+			}));
+
+			const expectedBody = blogPosts.map((post) => ({
+				_id: post._id.toString(),
+				user: post.user.toString(),
+				title: post.title,
+				content: post.content,
+				date: post.date,
+				__v: post.__v,
+			}));
+
+			expect(responseBody).toEqual(expectedBody);
 		});
 	});
 });
