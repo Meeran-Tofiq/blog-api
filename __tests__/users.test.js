cf1f5a12d56c050278338792e95f208a3660e77a
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-07-04 13:21:49 +0300

    Feat testing for logging in

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,10 +1,28 @@
-const { users } = require("../__mocks__/users.mock");
+const { users, userMockData } = require("../__mocks__/users.mock");
 const request = require("supertest");
 const app = require("../jest.setup");
 
 const baseUrl = "/api/users";
 
 describe("Users route", () => {
+	describe("POST", () => {
+		it("should return a JWT token, on successful login", async () => {
+			const [username, password] = [
+				users[0].username,
+				userMockData[0].password,
+			];
+			await request(app)
+				.post(baseUrl + "/login")
+				.type("form")
+				.send({
+					username,
+					password,
+				})
+				.expect(200)
+				.expect("Content-Type", /json/);
+		});
+	});
+
 	describe("GET", () => {
 		it("should give a 403 error if user not logged in", async () => {
 			await request(app)
